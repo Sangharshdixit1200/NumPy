906920a7270b638d2a71741a4d927c4c3d681ade
Sangharsh Dixit Rajput <35248612+Sangharshdixit1200@users.noreply.github.com>
2022-04-19 00:29:44 +0530

    NumPy

---
+++ Program.py
@@ -0,0 +1,10 @@
+import numpy as np
+from pandas import array
+arr = np.arange(99)
+print(arr)
+arr = arr.reshape(3,33)
+print(arr)
+ide = np.identity(45)
+print(ide)
+listarray = np.array([[3,4,5],[4,3,2],[23,2,3]]) 
+print(listarray)
